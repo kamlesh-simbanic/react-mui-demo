3f7beddbc2fa8a212a389327b3c37fc2a89530d6
kamlesh-simbanic <kamlesh.simbanic2022@gmail.com>
2022-12-01 18:21:52 +0530

    Button added

---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,110 @@
+import React from "react";
+import SendIcon from "@mui/icons-material/Send";
+import { Button, Stack, IconButton } from "@mui/material";
+
+export const MuiButton = () => {
+  return (
+    <Stack spacing={4}>
+      <Stack spacing={2} direction="row">
+        <Button variant="text" href="https://google.com">
+          Text
+        </Button>
+        <Button variant="contained">contained</Button>
+        <Button variant="outlined">outlined</Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" color="primary">
+          Priamry
+        </Button>
+        <Button variant="contained" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="contained" color="error">
+          Error
+        </Button>
+        <Button variant="contained" color="warning">
+          Warning
+        </Button>
+        <Button variant="contained" color="info">
+          Info
+        </Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="outlined" color="primary">
+          Priamry
+        </Button>
+        <Button variant="outlined" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+        <Button variant="outlined" color="warning">
+          Warning
+        </Button>
+        <Button variant="outlined" color="info">
+          Info
+        </Button>
+        <Button variant="outlined" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button variant="text" color="primary">
+          Priamry
+        </Button>
+        <Button variant="text" color="secondary">
+          Secondary
+        </Button>
+        <Button variant="text" color="error">
+          Error
+        </Button>
+        <Button variant="text" color="warning">
+          Warning
+        </Button>
+        <Button variant="text" color="info">
+          Info
+        </Button>
+        <Button variant="text" color="success">
+          Success
+        </Button>
+      </Stack>
+
+      <Stack display={"block"} spacing={2} direction="row">
+        <Button variant="contained" size="small">
+          Small
+        </Button>
+        <Button variant="contained" size="medium">
+          Medium
+        </Button>
+        <Button variant="contained" size="large">
+          Large
+        </Button>
+      </Stack>
+
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="contained"
+          startIcon={<SendIcon />}
+          disableRipple
+          onClick={() => alert("Clicked")}
+        >
+          Send
+        </Button>
+        <Button variant="contained" endIcon={<SendIcon />} disableElevation>
+          Send
+        </Button>
+
+        <IconButton aria-label="send" color="success" size="small">
+          <SendIcon />
+        </IconButton>
+      </Stack>
+    </Stack>
+  );
+};
