eb5b37fec5d6e6f9d47c98f51c202cab4bfc647d
kamlesh-simbanic <kamlesh.simbanic2022@gmail.com>
2022-12-02 17:48:23 +0530

    stack added

--- src/components/MuiLayout.tsx
+++ src/components/MuiLayout.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { Box } from "@mui/material";
+import { Box, Stack, Divider } from "@mui/material";
 
 export const MuiLayout = () => {
   return (
-    <>
+    <Stack
+      sx={{
+        border: "1px solid",
+      }}
+      direction="row"
+      spacing={2}
+      divider={<Divider orientation="vertical" flexItem />}
+    >
       <Box
         sx={{
           backgroundColor: "primary.main",
@@ -26,6 +33,6 @@ export const MuiLayout = () => {
         bgcolor="success.light"
         p={2}
       ></Box>
-    </>
+    </Stack>
   );
 };
